6f1a40bef1997ae8855321e9a7371b7151b71047
Javiuty <javiiescuadra@gmail.com>
2020-10-19 20:42:53 +0200

    finishing header

--- components/layout/Navegacion.js
+++ components/layout/Navegacion.js
@@ -1,19 +1,30 @@
 import React from "react";
 import Link from "next/link";
+import styled from "@emotion/styled";
+
+const Nav = styled.nav`
+  padding-left: 2rem;
+
+  a {
+    font-size: 1.8rem;
+    margin-left: 2rem;
+    color: var(--gris2);
+    font-family: "PT Sans", sans-serif;
+    text-decoration: none;
+
+    &:last-of-type {
+      margin-right: 0;
+    }
+  }
+`;
 
 const Navegacion = () => {
   return (
-    <nav>
-      <Link href="/">
-        <a>Inicio</a>
-      </Link>
-      <Link href="/">
-        <a>Populares</a>
-      </Link>
-      <Link href="/">
-        <a>Nuevo Producto</a>
-      </Link>
-    </nav>
+    <Nav>
+      <Link href="/">Inicio</Link>
+      <Link href="/populares">Populares</Link>
+      <Link href="/nuevo-producto">Nuevo Producto</Link>
+    </Nav>
   );
 };
 
--- components/ui/Buscar.js
+++ components/ui/Buscar.js
@@ -8,19 +8,34 @@ const InputText = styled.input`
   min-width: 300px;
 `;
 
-const InputSubmit = styled.input`
+const InputSubmit = styled.button`
   height: 3rem;
   width: 3rem;
   display: block;
   background-size: 4rem;
   background-image: url("/static/img/buscar.png");
+  background-repeat: no-repeat;
+  position: absolute;
+  right: 1rem;
+  top: 1px;
+  background-color: white;
+  border: none;
+  text-indent: -9999px;
+
+  &:hover {
+    cursor: pointer;
+  }
 `;
 
 const Buscar = () => {
   return (
-    <form>
-      <InputText type="text" />
-      <button type="submit">Buscar</button>
+    <form
+      css={css`
+        position: relative;
+      `}
+    >
+      <InputText type="text" placeholder="Buscar Productos" />
+      <InputSubmit type="submit">Buscar</InputSubmit>
     </form>
   );
 };
