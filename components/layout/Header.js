ac669b6dd320bc690255b4a09c55d188742105f3
Javiuty <javiiescuadra@gmail.com>
2020-10-19 17:49:11 +0200

    some config + global styles

---
+++ components/layout/Header.js
@@ -0,0 +1,32 @@
+import React from "react";
+import Buscar from "../ui/Buscar";
+import Navegacion from "./Navegacion";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <div>
+          <p>P</p>
+
+          <Buscar />
+          <Navegacion />
+        </div>
+        <div>
+          <p>Hola: Javi</p>
+          <button type="button">Cerrar Sesión</button>
+
+          <Link href="/">
+            <a>Login</a>
+          </Link>
+          <Link href="/">
+            <a>Crear Cuenta</a>
+          </Link>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
