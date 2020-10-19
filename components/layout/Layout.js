f5f8c37c0e1e670a2d5c55b04902f605d1e24e50
Javiuty <javiiescuadra@gmail.com>
2020-10-19 20:02:39 +0200

    adding style to the header

--- components/layout/Layout.js
+++ components/layout/Layout.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "../layout/Header";
 import { Global, css } from "@emotion/core";
+import Head from "next/head";
 
 const Layout = (props) => {
   return (
@@ -10,6 +11,7 @@ const Layout = (props) => {
           :root {
             --gris: #3d3d3d;
             --gris2: #6f6f6f;
+            --gris3: #e1e1e1;
             --naranja: #da552f;
           }
 
@@ -27,6 +29,7 @@ const Layout = (props) => {
           body {
             font-size: 1.6rem;
             line-height: 1.5;
+            font-family: 'PT Sans', sans-serif;
           }
 
           h1,
@@ -34,6 +37,15 @@ const Layout = (props) => {
           h3 {
             margin: 0 0 2rem 0;
             line-height: 1.5;
+          }
+
+          h1, h2 {
+            font-family: 'Roboto Slab", serif;
+            font-weight: 700;
+          }
+
+          h3 {
+            font-family: 'PT Sans', sans-serif;
           }
 
           ul {
@@ -47,6 +59,22 @@ const Layout = (props) => {
           }
         `}
       />
+      <Head>
+        <html lang="es" />
+        <title>Product Hunt Firebase y Next.js</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.min.css"
+          integrity="sha512-NhSC1YmyruXifcj/KFRWoC561YpHpc5Jtzgvbuzx5VozKpWvQ+4nXhPdFgmx8xqexRcpAglTj9sIBWINXa8x5w=="
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&family=Roboto+Slab:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+        <link href="/static/css/app.css" rel="stylesheet" />
+      </Head>
+
       <Header />
       <main>{props.children}</main>
     </>
--- components/ui/Buscar.js
+++ components/ui/Buscar.js
@@ -1,9 +1,25 @@
 import React from "react";
+import styled from "@emotion/styled";
+import { css } from "@emotion/core";
+
+const InputText = styled.input`
+  border: 1px solid var(--gris3);
+  padding: 1rem;
+  min-width: 300px;
+`;
+
+const InputSubmit = styled.input`
+  height: 3rem;
+  width: 3rem;
+  display: block;
+  background-size: 4rem;
+  background-image: url("/static/img/buscar.png");
+`;
 
 const Buscar = () => {
   return (
     <form>
-      <input type="text" />
+      <InputText type="text" />
       <button type="submit">Buscar</button>
     </form>
   );
