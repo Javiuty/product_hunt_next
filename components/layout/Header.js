4e58486e88318f9aad1bf8a86feb2d15e6e6da1c
Javiuty <javiiescuadra@gmail.com>
2020-10-20 16:16:52 +0200

    login and register finished

--- components/layout/Header.js
+++ components/layout/Header.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useContext } from "react";
 import Link from "next/link";
 import styled from "@emotion/styled";
 import { css } from "@emotion/core";
 import Buscar from "../ui/Buscar";
 import Navegacion from "./Navegacion";
 import Boton from "../ui/Boton";
+import { FirebaseContext } from "../../firebase";
 
 const ContenedorHeader = styled.div`
   max-width: 1200px;
@@ -26,7 +27,7 @@ const Logo = styled.p`
 `;
 
 const Header = () => {
-  const usuario = false;
+  const { usuario, firebase } = useContext(FirebaseContext);
 
   return (
     <header
@@ -63,9 +64,11 @@ const Header = () => {
                   margin-right: 2rem;
                 `}
               >
-                Hola: Javi
+                Hola: {usuario.displayName}
               </p>
-              <Boton bgColor="true">Cerrar Sesión</Boton>
+              <Boton bgColor="true" onClick={() => firebase.cerrarSesion()}>
+                Cerrar Sesión
+              </Boton>
             </>
           ) : (
             <>
